65b5827f08dfdb278df183409724b0a089ba4332
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-01 14:23:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 
 def dictionary 
-  {"hello" => "hi"
-"to, two, too" => "2"
-"for, four" => "4"
-"be" => "b"
-"you" => "u"
-"at" => "@" 
+  {"hello" => "hi",
+"to, two, too" => "2",
+"for, four" => "4",
+"be" => "b",
+"you" => "u",
+"at" => "@", 
 "and" => "&"}
 end 
 
