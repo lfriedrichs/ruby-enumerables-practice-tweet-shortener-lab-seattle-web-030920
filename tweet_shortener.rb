83ceb3a448cb1a7e11125f0a0934df75f94cefc8
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-01 14:28:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -24,7 +24,6 @@ def word_substituter(string)
 end 
 
 def bulk_tweet_shortener(array)
-  new_array = array.map {|tweet| 
-    word_substituter(tweet) }
-    puts new_array
+    array.map {|tweet| 
+    puts word_substituter(tweet) }
 end 
